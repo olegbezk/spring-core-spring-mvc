b1cf039fc300bd48fe022b96f6ab1a5be46965e1
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-02-17 13:25:27 +0200

    add spring security password encoder

--- src/main/java/guru/springframework/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/config/SpringSecurityConfig.java
@@ -1,11 +1,33 @@
 package guru.springframework.config;
 
+import org.jasypt.springsecurity3.authentication.encoding.PasswordEncoder;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor){
+        PasswordEncoder passwordEncoder = new PasswordEncoder();
+        passwordEncoder.setPasswordEncryptor(passwordEncryptor);
+        return passwordEncoder;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder,
+                                                               UserDetailsService userDetailsService){
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return daoAuthenticationProvider;
+    }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
