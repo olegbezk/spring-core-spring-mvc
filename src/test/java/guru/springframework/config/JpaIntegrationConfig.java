5bf3576d2002f6e8d8d993bcb11728fd6daa54a2
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2018-02-17 22:21:33 +0200

    add web security to test config class

--- src/test/java/guru/springframework/config/JpaIntegrationConfig.java
+++ src/test/java/guru/springframework/config/JpaIntegrationConfig.java
@@ -3,9 +3,11 @@ package guru.springframework.config;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableWebSecurity
 @ComponentScan("guru.springframework")
 public class JpaIntegrationConfig {
 
